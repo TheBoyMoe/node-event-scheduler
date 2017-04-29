7c34ffb51f725950addee29e6f86256154a4537b
bill fero <billfero@gmail.com>
2017-04-29 19:06:59 +0100

    impl signup form

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,8 @@ router.get('/', (req, res, next)=>{
     return res.render('login', {title: 'Login'});
 });
 
+router.get('/signup', (req, res, next)=>{
+    return res.render('signup', {title: 'Signup'});
+});
+
 module.exports = router;
