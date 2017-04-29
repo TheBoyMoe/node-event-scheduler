677620d6ef7779499e1afac0781a7bfadaa7c19e
bill fero <billfero@gmail.com>
2017-04-29 19:27:30 +0100

    add login and signup post routes - returns simple message to confirm working

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,22 @@ router.get('/', (req, res, next)=>{
     return res.render('login', {title: 'Login'});
 });
 
+// GET /signup
 router.get('/signup', (req, res, next)=>{
     return res.render('signup', {title: 'Signup'});
 });
+
+
+// POST / - handle login submission form data
+router.post('/', (req, res, next)=>{
+    return res.send('User logged in');
+});
+
+// POST /signup - handle submission of signup form data
+router.post('/signup', (req, res, next)=>{
+    return res.send('User created');
+});
+
+
 
 module.exports = router;
